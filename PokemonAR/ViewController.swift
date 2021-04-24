5ae84308f5bd3fc69cc116703bd8fdf88259b08e
Shubham Mishra <shubhammishra10@gmail.com>
2021-04-24 09:06:14 +0530

    ADD: Card image detection and place plane over it

--- PokemonAR/ViewController.swift
+++ PokemonAR/ViewController.swift
@@ -21,20 +21,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        let configuration = ARImageTrackingConfiguration()
+        guard let imagesToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: .main) else {
+            fatalError("Failed to load tracking images")
+        }
+        
+        configuration.trackingImages = imagesToTrack
+        configuration.maximumNumberOfTrackedImages = 2
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -48,27 +47,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
         let node = SCNNode()
-     
+        guard let imageAnchor = anchor as? ARImageAnchor else { return nil}
+
+        let refImage = imageAnchor.referenceImage
+
+        let imagePlane = SCNPlane(width: refImage.physicalSize.width, height: refImage.physicalSize.height)
+
+        let planeNode = SCNNode(geometry: imagePlane)
+        planeNode.eulerAngles.x = -.pi/2
+        planeNode.opacity = 0.5
+        
+        node.addChildNode(planeNode)
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
+        let configuration = ARImageTrackingConfiguration()
+        guard let imagesToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: .main) else {
+            fatalError("Failed to load tracking images")
+        }
+        
+        configuration.trackingImages = imagesToTrack
+        configuration.maximumNumberOfTrackedImages = 2
+        // Run the view's session
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
         
     }
 }
